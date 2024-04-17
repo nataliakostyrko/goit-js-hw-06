69f9c9c18f3f93e189cea8435bc92eadeb659eea
nataliakostyrko <nataliskrip1408@gmail.com>
2024-04-17 19:09:15 +0200

    commit

--- js/task-3.js
+++ js/task-3.js
@@ -1,11 +1,31 @@
-function getElementWidth(content, padding, border) {
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+class StringBuilder {
+    #value;
 
-    return contentWidth + paddingWidth * 2 + borderWidth * 2;
+    constructor(value) {
+        this.#value = value;
+    }
+
+    getValue() {
+        return this.#value;
+    }
+
+    padEnd(str) {
+        this.#value = this.#value + str;
+    }
+    padStart(str) {
+        this.#value = str + this.#value;
+    }
+
+    padBoth(str) {
+        this.padEnd(str);
+        this.padStart(str);
+    }
 }
-
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+const builder = new StringBuilder(".");
+console.log(builder.getValue()); // "."
+builder.padStart("^");
+console.log(builder.getValue()); // "^."
+builder.padEnd("^");
+console.log(builder.getValue()); // "^.^"
+builder.padBoth("=");
+console.log(builder.getValue()); // "=^.^="
